d65c74ad20c960729d13aa2fe418eef6bfa6e89e
Aryaman Chakraborty <72442257+aryaman911@users.noreply.github.com>
2025-12-08 19:06:23 -0500

    Update API_BASE_URL to new backend URL

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // js/config.js
 // CHANGE THIS to your actual Render backend URL
-export const API_BASE_URL = "https://moviehub-backend.onrender.com"; 
+export const API_BASE_URL = "https://netflix-hub-backend.onrender.com"; 
 // no trailing slash
